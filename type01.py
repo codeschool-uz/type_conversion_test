cc4656aba81c1a1b8a5891c70f8caba375b63b61
ZARIF ubuntu <uzbzarif@gmail.com>
2021-11-07 15:26:43 +0900

    test01

---
+++ type01.py
@@ -0,0 +1,10 @@
+#Create a variable called 'var' and assign with data type integer to it.
+var=3
+#Convert 'var' to float data type and assign to another variable called 'var_float'
+var_float=float(var)
+#Convert 'var' to string data type and assign to another variable called 'var_str'
+var_str=str(var)
+#Print the value of 'var_float'
+print(var_float)
+#Print the value of 'var_str'
+print(var_str)
